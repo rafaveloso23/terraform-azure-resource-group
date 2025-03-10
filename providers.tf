fce764f2d805e78023fe71ed770ca306804d1a11
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-03-10 20:33:17 -0300

    teds

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "azurerm" {
     subscription_id = "cc323661-bdfb-4e37-8224-b9f41308d182"
+    client_id       = "1db7c94a-a6f8-42e3-ba60-6c3b1b2dfaf4"
+    client_secret   = "DrG8Q~STWYa48bp0Q8lrnl5tJ9OfvoSXczXTfc.X"
+    tenant_id       = "0eed3ea8-f35c-4862-b14a-9809318064c7"
     features {}
 }
 #ss
