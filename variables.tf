e7dd82af5aa0ab1c5e2b77644d59a9dce527e3c8
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-03-15 16:04:18 -0300

    ad

--- variables.tf
+++ variables.tf
@@ -16,19 +16,6 @@ variable "tags" {
   }
 }
 
-
-variable "client_id" {
-  type = string
-}
-
 variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
   type = string
 }
