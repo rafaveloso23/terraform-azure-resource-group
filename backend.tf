7b5a38f5140f61b696f9f3002fcae5dd715bc3ba
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-03-22 22:54:40 -0300

    ad

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  cloud {
-    organization = "veloso"
-    hostname     = "app.terraform.io"
+# terraform {
+#   cloud {
+#     organization = "veloso"
+#     hostname     = "app.terraform.io"
 
-    workspaces {
-      project = "modules"
-      name    = "terraform-azure-resource-group"
-    }
-  }
-}
+#     workspaces {
+#       project = "modules"
+#       name    = "terraform-azure-resource-group"
+#     }
+#   }
+# }
