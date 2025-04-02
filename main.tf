0531cd42d65d2073025b35b18dbbf871a30bc495
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-04-01 22:15:30 -0300

    qe

--- main.tf
+++ main.tf
@@ -3,4 +3,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.tags
 }
+
+
+resource "azurerm_storage_account" "name" {
+  name                     = "rgfrrrewr"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags                    = var.tags
+}
 ##
