d4cd18faf51159e9fe47d126317d74e56d490132
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-04-18 15:36:53 -0300

    addd

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-  tags     = var.tags
+module "resource-group" {
+  source  = "app.terraform.io/veloso/resource-group/azure"
+  version = "1.0.1"
+  client_secret = var.client_secret
+  resource_group_name = "rg-teste"
+  # insert required variables here
+  depends_on = [ azurerm_resource_group.name ]
 }
-######
+resource "azurerm_resource_group" "name" {
+  name = "rg"
+  location = "East US"
+}
