d55ed78f789479e509a5f903785bd27d4cf42d71
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-04-18 15:40:09 -0300

    aad

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "resource-group" {
   version = "1.0.1"
   client_secret = var.client_secret
   resource_group_name = "rg-teste"
-  # insert required variables here
   depends_on = [ azurerm_resource_group.name ]
 }
 resource "azurerm_resource_group" "name" {
