ae0cc4e3de8b833b1aeca62e406f3a7b67047e40
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-04-19 12:29:31 -0300

    a

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ module "resource-group" {
   resource_group_name = "rg-teste"
   depends_on = [ azurerm_resource_group.name ]
 }
+
+module "storage" {
+  source  = "app.terraform.io/veloso/storage/azure"
+  version = "1.0.0"
+  rg_location = module.resource-group.resource_group_location
+  rg_name     = module.resource-group.resource_group_name
+}
+
 resource "azurerm_resource_group" "name" {
   name = "rg"
   location = "East US"
