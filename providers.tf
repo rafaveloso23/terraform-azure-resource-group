d6e31f7a983500c14abdf61e1e15f11d45edd1db
Rafael Veloso <rafa.veloso.d.s@gmail.com>
2025-04-24 22:44:57 -0300

    a

--- providers.tf
+++ providers.tf
@@ -4,4 +4,4 @@ provider "azurerm" {
   client_id       = "07c8c7c7-647f-4c2f-92f9-41326eef863a"
   client_secret   = var.client_secret
   tenant_id       = "0eed3ea8-f35c-4862-b14a-9809318064c7"
-}
+}#
